04ae46a7eb0f56645ea0a09ab9583275be091202
JacobLuqueEntenza <jacobluquentenza@gmail.com>
2024-11-08 12:37:08 +0100

    Creada aqui la rama trasteros para aprovechar las carpetas que no cree en el main. Solo falta que funcion los videos y botones

---
+++ app/modelo/trasterosModelo.php
@@ -0,0 +1,73 @@
+<?php
+/**
+     * conexion a la base de datos
+     * @return usuarios PDO
+     */
+
+
+class TrasterosModelo
+{
+ 
+ private $db;
+
+ /**
+ * Constructor de la clase.
+ *
+ * Inicializa la conexión a la base de datos.
+ */
+ public function __construct() {
+	// Inicializa la conexión
+    $this->db =new Conexion(); 
+    $this->db->conectar();
+	//
+  }
+
+
+/**
+ * Obtiene un listado paginado de trasteros.
+ *
+ * Este método devuelve un array con los datos de los trasteros.
+ *
+ * @return array Array asociativo con los datos de los trasteros.
+ */
+    public function getListaTrasteros(){
+		
+		$sql="  SELECT * 
+                FROM trasteros 
+                ORDER BY id_trastero ASC
+                LIMIT 23";
+		$conectar=$this->db->conectar();
+		$consulta=$conectar->prepare($sql);
+        $consulta->execute();
+        $trastero=$consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $trastero;        
+    }//fin lista de trasteros
+
+
+
+    /**
+    * Obtiene un listado paginado de trasteros asociado a cada cliente que tiene un trastero
+    *
+    * Este método devuelve un array con los datos de los trasteros.
+    *
+    * @return array Array asociativo con los datos de los trasteros.
+    */
+    public function getListaTrasterosAdmin(){
+		
+		$sql="  SELECT DISTINCT t.*,u.nombre,u.apellido_1,u.      apellido_2
+                FROM recibos r 
+                INNER JOIN users u on u.id_user=r.user_id
+                INNER JOIN trasteros t on t.id_trastero=r.trastero_id
+                ORDER BY id_trastero ASC
+                LIMIT 23";
+		$conectar=$this->db->conectar();
+		$consulta=$conectar->prepare($sql);
+        $consulta->execute();
+        $trastero=$consulta->fetchAll(PDO::FETCH_ASSOC);
+
+        return $trastero;        
+    }//fin lista de trasteros
+
+    
+}
--- app/vista/layouts/header.php
+++ app/vista/layouts/header.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="/trasteros1024/public/css/estilos.css">
     <!-- Enlaza los estilos de Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">      
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">  
+    <script src="/trasteros1024/public/js/script.js"></script>    
 </head>
 
 <body class="hidden">
